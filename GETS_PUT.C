d6fdb779c18e5caaa27355470c1f85c32df3cb0a
Meher Mankar <84778567+mehermankarofficial@users.noreply.github.com>
2022-06-26 20:25:22 +0530

    Add files via upload

---
+++ GETS_PUT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char name[15];
+clrscr();
+puts("Enter your name :");
+gets(name); // gets() is a function is used to receive a string from the keyboard
+puts(name); // puts() is used to write a string to the screen
+getch();
+}
